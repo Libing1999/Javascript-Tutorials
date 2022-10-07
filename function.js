69a3907e7f5e215b7c559046a03a60128fdc5c18
Libing1999 <libin@strebentechnik.com>
2022-10-07 15:44:22 +0530

    bulbonnoff

---
+++ function.js
@@ -0,0 +1,9 @@
+
+function bulbon() {
+    document.getElementById("bulb").src = "images/bulb2.png";
+    document.getElementById("bulbStatus").innerHTML = "Bulb in now ON!";
+}
+function bulboff() {
+    document.getElementById("bulb").src = "images/bulb1.png";
+    document.getElementById("bulbStatus").innerHTML = "Bulb in now OFF!";
+}
