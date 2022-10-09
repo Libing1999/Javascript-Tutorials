7dd88a1b76eb36ac7c1001e1884d6fb6debfbf96
Libing1999 <libin@strebentechnik.com>
2022-10-09 15:05:03 +0530

    bulbonn/off

--- function.js
+++ function.js
@@ -1,9 +1,11 @@
 
-function bulbon() {
-    document.getElementById("bulb").src = "images/bulb2.png";
-    document.getElementById("bulbStatus").innerHTML = "Bulb in now ON!";
-}
-function bulboff() {
-    document.getElementById("bulb").src = "images/bulb1.png";
-    document.getElementById("bulbStatus").innerHTML = "Bulb in now OFF!";
+const bulbOn = () => {
+    let bid = document.getElementById('bulb');
+    if (bid.src.match('bulb2')) {
+        bid.src = "/images/bulb1.png";
+        document.getElementById("bulbStatus").innerHTML = "Bulb in now OFF!";
+    } else {
+        bid.src = "/images/bulb2.png";
+        document.getElementById("bulbStatus").innerHTML = "Bulb in now ON!";
+    }
 }
